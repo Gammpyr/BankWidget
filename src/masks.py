e278546500c9323440410274c61b9577be48196b
Gamid Abdulkhalikov <malak93@yandex.ru>
2025-03-13 20:52:09 +0300

    git init

---
+++ src/masks.py
@@ -0,0 +1,16 @@
+def get_mask_card_number(card_number: int | str) -> str:
+    """Принимает номер карты, возвращает её маску"""
+    number_list = str(card_number)
+    result = f'{number_list[:4]} {number_list[4:6]}** **** {number_list[-4:]}'
+    return result
+
+
+def get_mask_account(account_number: int | str) -> str:
+    """Принимает номер счета и возвращает маску формата **XXXX"""
+    str_account_number = str(account_number)
+    return f"**{str_account_number[-4:]}"
+
+
+if __name__ == "__main__":
+    print(get_mask_card_number(input()))
+    print(get_mask_account(input()))
---
+++ src/widget.py
@@ -0,0 +1,34 @@
+from datetime import datetime
+
+from src.masks import get_mask_account, get_mask_card_number
+
+
+def mask_account_card(card_info: str) -> str:
+    """Функция определяет тип карты/счёта и возвращает соответствующую маску"""
+    if "Счет" in card_info:
+        account_mask = "Счет " + get_mask_account(card_info[5:])
+        return account_mask
+    else:
+        card_mask = card_info[:-16] + get_mask_card_number(card_info[-16:])
+        return card_mask
+
+
+def get_time(date: str) -> str:
+    """Функция получает время и возвращает в формате ДД.ММ.ГГГГ"""
+    get_datetime = datetime.strptime(date, "%Y-%m-%dT%H:%M:%S.%f")
+    correct_date = get_datetime.strftime('%d.%m.%Y')
+    return correct_date
+
+
+if __name__ == "__main__":
+    print(mask_account_card("Maestro 1596837868705199"))
+    print(mask_account_card("MasterCard 7158300734726758"))
+    print(mask_account_card("Visa Classic 6831982476737658"))
+    print(mask_account_card("Visa Platinum 8990922113665229"))
+    print(mask_account_card("Visa Gold 5999414228426353"))
+
+    print(mask_account_card("Счет 64686473678894779589"))
+    print(mask_account_card("Счет 35383033474447895560"))
+    print(mask_account_card("Счет 73654108430135874305"))
+
+    print(get_time('2024-03-11T02:26:18.671407'))
