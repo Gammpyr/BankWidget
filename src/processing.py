b5a83144caa56e4c47bcd00fa85e9b3af8541855
Gamid Abdulkhalikov <malak93@yandex.ru>
2025-03-28 21:01:49 +0300

    update processing.py

--- src/processing.py
+++ src/processing.py
@@ -13,27 +13,3 @@ def sort_by_date(dict_list: list[dict], sort_order: bool = True) -> list[dict]:
     И возвращает новый список, отсортированный по дате"""
     sorted_result = sorted(dict_list, key=lambda x: x["date"], reverse=sort_order)
     return sorted_result
-
-
-if __name__ == "__main__":
-    print(
-        filter_by_state(
-            [
-                {"id": 41428829, "state": "EXECUTED", "date": "2019-07-03T18:35:29.512364"},
-                {"id": 939719570, "state": "EXECUTED", "date": "2018-06-30T02:08:58.425572"},
-                {"id": 594226727, "state": "CANCELED", "date": "2018-09-12T21:27:25.241689"},
-                {"id": 615064591, "state": "CANCELED", "date": "2018-10-14T08:21:33.419441"},
-            ]
-        )
-    )
-
-    print(
-        sort_by_date(
-            [
-                {"id": 41428829, "state": "EXECUTED", "date": "2019-07-03T18:35:29.512364"},
-                {"id": 939719570, "state": "EXECUTED", "date": "2018-06-30T02:08:58.425572"},
-                {"id": 594226727, "state": "CANCELED", "date": "2018-09-12T21:27:25.241689"},
-                {"id": 615064591, "state": "CANCELED", "date": "2018-10-14T08:21:33.419441"},
-            ]
-        )
-    )
