f41a722b7ae53be10a1b22aa5bb1800f97363f08
Gamid Abdulkhalikov <malak93@yandex.ru>
2025-04-21 19:24:41 +0300

    update tests

--- tests/test_expernal_api.py
+++ tests/test_expernal_api.py
@@ -1,44 +1,36 @@
-from unittest.mock import patch
+from typing import Any
+from unittest.mock import mock_open, patch
 
 import pytest
 
 from src.external_api import convert_to_rub
 
 
-@patch('requests.get')
-def test_convert_to_rub(mock_get):
+@patch("requests.get")
+@patch("builtins.open", new_callable=mock_open)
+def test_convert_to_rub(mock_file: Any, mock_get: Any) -> None:
+    mock_file.return_value.__enter__.return_value.read.return_value = '{"some": "data"}'
     mock_get.return_value.status_code = 200
     mock_get.return_value.json.return_value = {
-        "query": {
-            "from": "USD",
-            "amount": 1
-        },
-        "info": {
-            "rate": 2
-        },
+        "query": {"from": "USD", "amount": 1},
+        "info": {"rate": 2},
         "date": "2000-01-01",
-        "result": 3.3
+        "result": 3.3,
     }
 
-    result = convert_to_rub('USD', 2)
+    result = convert_to_rub("USD", 2)
     assert result == 3.3
 
-@patch('requests.get')
-def test_convert_to_rub_no_currency(mock_get):
+
+@patch("requests.get")
+def test_convert_to_rub_no_currency(mock_get: Any) -> None:
     mock_get.return_value.status_code = 800
     mock_get.return_value.json.return_value = {
-        "query": {
-            "from": "USD",
-            "amount": 1
-        },
-        "info": {
-            "rate": 2
-        },
+        "query": {"from": "USD", "amount": 1},
+        "info": {"rate": 2},
         "date": "2000-01-01",
-        "result": 3.3
+        "result": 3.3,
     }
 
     with pytest.raises(ValueError, match="Failed to get currency rate. Status code: 800"):
-        convert_to_rub('USD', 2)
-
-
+        convert_to_rub("USD", 2)
--- tests/test_utils.py
+++ tests/test_utils.py
@@ -1,27 +1,28 @@
 import json
-from unittest.mock import patch, mock_open
+from typing import Any
+from unittest.mock import mock_open, patch
 
-
-from src.utils import get_data_from_json, get_amount_in_rubles
-from src.external_api import convert_to_rub
+from src.utils import get_amount_in_rubles, get_data_from_json
 
 json_file = [
     {
         "operationAmount": {
             "amount": "31957.58",
-            "currency": {
-                "name": "руб.",
-                "code": "RUB"
-            },
+            "currency": {"name": "руб.", "code": "RUB"},
         },
     }
 ]
 
 
-@patch('builtins.open', new_callable=mock_open, read_data=json.dumps(json_file))
-def test_get_data_from_json(mock_open):
-    result = get_data_from_json('fake_file.json')
+@patch("builtins.open", new_callable=mock_open, read_data=json.dumps(json_file))
+def test_get_data_from_json(mock_open: Any) -> None:
+    result = get_data_from_json("fake_file.json")
 
     assert result == json_file
 
 
+@patch("builtins.open", new_callable=mock_open, read_data='{"date": "2025-04-21", "info": {"rate": 3.0}}')
+def test_get_amount_in_rubles(mock_file: Any) -> None:
+    test_json_file = {"operationAmount": {"currency": {"code": "USD"}, "amount": "2.0"}}
+    result = get_amount_in_rubles(test_json_file)
+    assert result == 6.0
